cf0c9b7d694399d089996c016f63909f0423f2f7
Asude Bagatur <asude.bagatur@yandex.ru>
2023-10-12 23:17:20 +0300

    Debug False and add allowed_hosts for taski

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,3 +1,4 @@
+
 import os
 from pathlib import Path
 
@@ -6,9 +7,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.75.29', '127.0.0.1', 'localhost', 'trialhostname.hopto.org']
 
 
 # Application definition
